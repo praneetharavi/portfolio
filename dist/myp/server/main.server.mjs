c33e7080cbebd9c73f0064bcfe7c7547a2ba3502
Praneetha Ravi <pravi2@albany.edu>
2024-05-01 20:13:18 -0400

    build changes

--- dist/myp/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-JPREZMU7.mjs";import"./chunk-KRLG4YFP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- dist/portfolio/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-S4MURY6I.mjs";import"./chunk-2SOEMFXK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
